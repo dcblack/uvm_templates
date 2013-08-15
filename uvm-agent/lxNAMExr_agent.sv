5a22eeab7a7368dc298ef45fd3b98f29e63b32b3
David C Black <dcblack@hldwizard.com>
2013-08-15 15:30:33 -0500

    Fixed per updated newscript & tested

--- uvm-agent/lxNAMExr_agent.sv
+++ uvm-agent/lxNAMExr_agent.sv
@@ -1,7 +1,7 @@
 class {:NAME:}_agent extends uvm_component;
 
   // Data attributes
-  {:NAME:}_agent_config m_cfg; //< fetched from config DB as "{:NAME:}_agent_config"
+  {:CONFIG:} m_cfg; //< fetched from config DB as "{:CONFIG:}"
   bit coverage_enable = 1;
   bit check_enable = 1;
   `uvm_component_utils_begin({:NAME:}_agent)
@@ -9,16 +9,18 @@ class {:NAME:}_agent extends uvm_component;
     `uvm_field_int(coverage_enable, UVM_ALL_ON)
   `uvm_component_utils_end
 
-  typedef uvm_sequencer#({:TRANSACTION:}) {:SEQUENCER:}_sequencer;
+  typedef uvm_sequencer#({:TRANSACTION:}) {:SEQUENCER:};
 
   // Component attributes
+  {:if:NO_TYPEDEF:}
   uvm_analysis_port #({:TRANSACTION:}) analysis_port;
+  {:endif:NO_TYPEDEF:}
   {:NAME:}_monitor          m_monitor;
-  {:SEQUENCER:}_sequencer        m_sequencer;
-  {:DRIVER:}_driver           m_driver;
-  {:if:COVERAGE:}
+  {:SEQUENCER:}        m_sequencer;
+  {:DRIVER:}           m_driver;
+  {:if:ADD_COVERAGE:}
   {:COVERAGE:} m_coverage;
-  {:endif:COVERAGE:}
+  {:endif:ADD_COVERAGE:}
 
   // Standard Methods
   extern function new(string _name = "{:NAME:}_agent", uvm_component _parent = null);
@@ -39,19 +41,21 @@ endfunction: new
 
 function void {:NAME:}_agent::build_phase( uvm_phase phase );
   super.build_phase(phase);
-  if( !uvm_config_db #( {:NAME:}_agent_config )::get(this,"{:NAME:}_agent_config",m_cfg) ) `uvm_error({MSGID,"Failed to get agent's config object")
+  if( !uvm_config_db #( {:CONFIG:} )::get(this,"{:CONFIG:}",m_cfg) ) begin 
+    `uvm_error({MSGID,"Failed to get agent's config object: {:CONFIG:}")
+  end
   // Monitor is always present
-  m_monitor = {:NAME:}_monitor::type_id::create("m_monitor", this);
+  m_monitor = {:MONITOR:}::type_id::create("m_monitor", this);
   // Only build the driver and sequencer if active
   if (get_is_active() == UVM_ACTIVE) begin
     m_driver    = {:DRIVER:}_driver::type_id::create   ("m_driver",    this);
     m_sequencer = {:SEQUENCER:}_sequencer::type_id::create("m_sequencer", this);
   end
-  {:if:COVERAGE:}
+  {:if:ADD_COVERAGE:}
   if(coverage_enable) begin
     m_coverage = {:COVERAGE:}::type_id::create("m_coverage", this);
   end
-  {:endif:COVERAGE:}
+  {:endif:ADD_COVERAGE:}
 endfunction: build_phase
 
 function void {:NAME:}_agent::connect_phase( uvm_phase phase );
@@ -62,11 +66,11 @@ function void {:NAME:}_agent::connect_phase( uvm_phase phase );
     m_driver.seq_item_port.connect(m_sequencer.seq_item_export);
     m_driver.{:NAME:}_vif = m_cfg.{:NAME:}_vif;
   end
-  {:if:COVERAGE:}
+  {:if:ADD_COVERAGE:}
   if(coverage_enable) begin
     m_monitor.analysis_port.connect(m_coverage.analysis_export);
   end
-  {:endif:COVERAGE:}
+  {:endif:ADD_COVERAGE:}
 endfunction :connect_phase
 
 //TAF!
